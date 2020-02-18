04c4e0f4d90586794baa4cfa5f83342d81e2940b
TOMAS O CONGHAILE - STUDENT <G00366058@gmit.ie>
2020-02-18 17:47:22 +0000

    Lab 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  count:number=0;
+  hideLabel:boolean = true;
+
+  buttonPressed(){
+    console.log("Button Pressed");
+    this.count++;
+  }
+
+
+  imageClicked(){
+    console.log("image double clicked")
+    if(this.hideLabel == true)
+    {
+      this.hideLabel = false;
+    }
+    else
+    {
+      this.hideLabel = true;
+    }
+    
+  }
 }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  presidents: string[] = ["Higgins", "McAleese", "Robinson", "Hillery", " O Dalaigh"];
+
+  mypresidents: object[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
